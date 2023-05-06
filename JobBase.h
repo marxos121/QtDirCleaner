886f6ed313dcfbcb75e1f20db7b201add442d02b
marxos121 <marxos121@gmail.com>
2023-05-06 21:06:49 +0100

    design JobBase class

---
+++ JobBase.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <filesystem>
+#include <unordered_set>
+#include <string>
+
+enum JobType {
+    Move, Remove
+};
+
+class JobBase {
+public:
+    JobBase(JobType l_type);
+    JobBase(std::istream& is);
+    virtual ~JobBase() = default;
+
+    void saveLog() const;
+    virtual bool isValid() const;
+
+    virtual void readIn(std::istream& is) = 0;
+    virtual void execute() = 0;
+
+protected:
+	std::filesystem::path m_targetDir;
+	std::unordered_set<std::string> m_targetExtensions;
+    std::unordered_set<std::string> m_exemptFiles;
+
+    bool m_isFinished;
+    const JobType m_type;
+    std::string m_log;
+
+public: 
+    //Setters and getters
+    JobType getType() const;
+    const std::string& getLog() const;
+
+    void setFinished(bool l_finished);
+    bool getFinished() const;
+
+    void setTargetDirectory(const std::filesystem::path& l_path);
+    const std::filesystem::path& getTargetDirectory() const;
+
+    void setTargetExtensions(const std::unordered_set<std::string>& l_extensions);
+    const std::unordered_set<std::string>& getTargetExtensions() const;
+};
