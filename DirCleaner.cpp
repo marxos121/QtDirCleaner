9822c90f4cc30e9f0579a2b6de9ae852a3264c70
marxos121 <marxos121@gmail.com>
2023-05-16 20:25:04 +0100

    implement DirCleaner class

---
+++ DirCleaner.cpp
@@ -0,0 +1,157 @@
+#include "DirCleaner.h"
+
+#include "JobBase.h"
+#include "JobMove.h"
+#include "JobRemove.h"
+
+#include <iostream>
+#include <fstream>
+
+DirCleaner::DirCleaner()
+{
+	registerJobType<JobMove>(JobType::Move);
+	registerJobType<JobRemove>(JobType::Remove);
+}
+
+DirCleaner::~DirCleaner()
+{
+	while (!m_jobs.empty())
+	{
+		delete m_jobs.front();
+		m_jobs.pop_front();
+	}
+}
+
+void DirCleaner::readInJobs(const std::wstring& l_path)
+{
+	std::wifstream is(l_path);
+
+	if (!is.is_open())
+	{
+		std::wcerr << "! Failed to open file: " << l_path << std::endl;
+		return;
+	}
+
+	std::wstring line;
+	while (std::getline(is, line))
+	{
+		std::wstring temp;
+		std::wistringstream oss(line);
+
+		oss >> temp;
+
+		if (temp == L"[TYPE]")
+		{
+			oss >> temp;
+
+			JobType type = strToJobType(temp);
+
+			if (type == (JobType)-1)
+			{
+				continue;
+			}
+
+			m_jobs.push_back(m_jobFactory[type]());
+			continue;
+		}
+
+		if (m_jobs.empty())
+		{
+			continue;
+		}
+
+		if (temp == L"[TARGET]")
+		{
+			oss >> temp;
+			m_jobs.back()->addTargetDirectory(temp);
+		}
+
+		else if (temp == L"[EXT]")
+		{
+			oss >> temp;
+			m_jobs.back()->addTargetExtension(temp);
+		}
+
+		else if (temp == L"[EXEMPT]")
+		{
+			oss >> temp;
+			m_jobs.back()->addExemptFile(temp);
+		}
+
+		else if (temp == L"[DEST]" && m_jobs.back()->getType() == JobType::Move)
+		{
+			auto currentJob = static_cast<JobMove*>(m_jobs.back());
+
+			if (!currentJob->getDestinationPath().empty())
+			{
+				continue;
+			}
+
+			oss >> temp;
+			currentJob->setDestinationPath(temp);
+		}
+	}
+}
+
+void DirCleaner::addJob(JobBase* l_jb)
+{
+	JobBase* temp = nullptr;
+	if (l_jb->getType() == JobType::Move) 
+	{
+		temp = new JobMove(*static_cast<JobMove*>(l_jb));
+	}
+	else if (l_jb->getType() == JobType::Remove)
+	{
+		temp = new JobRemove(*static_cast<JobRemove*>(l_jb));
+	}
+	m_jobs.push_back(l_jb);
+}
+
+void DirCleaner::executeNext()
+{
+	if (m_jobs.empty())
+	{
+		return;
+	}
+
+	m_jobs.front()->execute();
+	m_log += m_jobs.front()->getLog() + '\n';
+	m_jobs.pop_front();
+
+	++m_executedJobs;
+}
+
+void DirCleaner::executeAll() 
+{
+	while (!m_jobs.empty())
+	{
+		executeNext();
+	}
+}
+
+void DirCleaner::saveLog()
+{
+	m_log += "\n\nExecuted " + std::to_string(m_executedJobs) + " jobs.";
+	std::ofstream os("DirCleanerLog.txt", std::ios_base::app);
+	os << m_log;
+	os.close();
+}
+
+void DirCleaner::clearLogFile()
+{
+	std::ofstream os("DirCleanerLog.txt");
+	os.close();
+}
+
+JobType DirCleaner::strToJobType(const std::wstring& type) 
+{
+	if (type == L"move") {
+		return JobType::Move;
+	}
+
+	if (type == L"remove") {
+		return JobType::Remove;
+	}
+
+	return (JobType)-1;
+}
---
+++ DirCleaner.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <deque>
+#include <string>
+#include <functional>
+
+#include "JobBase.h"
+
+class DirCleaner {
+public:
+	DirCleaner();
+	~DirCleaner();
+
+	void readInJobs(const std::wstring& l_path = L"jobs.txt");
+	void addJob(JobBase* l_jb);
+
+	void executeNext();
+	void executeAll();
+
+	void saveLog();
+	void clearLogFile();
+
+private:
+	std::deque<JobBase*> m_jobs;
+	std::string m_log;
+	int m_executedJobs;
+
+	//Factory
+	std::unordered_map<JobType, std::function<JobBase* ()>> m_jobFactory;
+	template <typename T>
+	void registerJobType(JobType l_type)
+	{
+		auto fact = []()
+		{
+			return new T();
+		};
+
+		m_jobFactory.emplace(l_type, fact);
+	}
+
+	JobType strToJobType(const std::wstring& type);
+};
